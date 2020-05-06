187301c537327b585414dad161fd02d821b08f7e
Mr.Yosapat Thamarongrak <yosapat.trk@gmail.com>
2020-05-06 20:30:08 +0700

    Update HomeHeaderBanner.vue

--- portfolio/.vuepress/components/HomeHeaderBanner.vue
+++ portfolio/.vuepress/components/HomeHeaderBanner.vue
@@ -515,51 +515,52 @@ a {
 </style>
 
 <script>
+export default {
+  mounted: function() {
+    var msImages = new MomentumSlider({
+      // Element to append the slider
+      el: '.sliders-container',
+      // CSS class to reference the slider
+      cssClass: 'ms--images',
+      // Generate the 4 slides required
+      range: [0, 3],
+      rangeContent: function () {
+          return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+      },
+      // Syncronize the other sliders
+      sync: [msNumbers, msTitles, msLinks],
+      // Styles to interpolate as we move the slider
+      style: {
+          '.ms-slide__image': {
+              transform: [{scale: [1.5, 1]}]
+          }
+      }
+    });
 
-// Initializing the images slider
-var msImages = new MomentumSlider({
-    // Element to append the slider
-    el: '.sliders-container',
-    // CSS class to reference the slider
-    cssClass: 'ms--images',
-    // Generate the 4 slides required
-    range: [0, 3],
-    rangeContent: function () {
-        return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
-    },
-    // Syncronize the other sliders
-    sync: [msNumbers, msTitles, msLinks],
-    // Styles to interpolate as we move the slider
-    style: {
-        '.ms-slide__image': {
-            transform: [{scale: [1.5, 1]}]
-        }
-    }
-});
+    // Get pagination items
+    var pagination = document.querySelector('.pagination');
+    var paginationItems = [].slice.call(pagination.children);
 
-// Get pagination items
-var pagination = document.querySelector('.pagination');
-var paginationItems = [].slice.call(pagination.children);
+    // Update initialization for images slider
+    var msImages = new MomentumSlider({
+      // MORE OPTIONS
 
-// Update initialization for images slider
-var msImages = new MomentumSlider({
-    // MORE OPTIONS
+      // Update pagination if slider change
+      change: function(newIndex, oldIndex) {
+          if (typeof oldIndex !== 'undefined') {
+              paginationItems[oldIndex].classList.remove('pagination__item--active');
+          }
+          paginationItems[newIndex].classList.add('pagination__item--active');
+      }
+    });
 
-    // Update pagination if slider change
-    change: function(newIndex, oldIndex) {
-        if (typeof oldIndex !== 'undefined') {
-            paginationItems[oldIndex].classList.remove('pagination__item--active');
-        }
-        paginationItems[newIndex].classList.add('pagination__item--active');
-    }
-});
-
-// Select corresponding slider item when a pagination button is clicked
-pagination.addEventListener('click', function(e) {
-    if (e.target.matches('.pagination__button')) {
-        var index = paginationItems.indexOf(e.target.parentNode);
-        msImages.select(index);
-    }
-});
-
+    // Select corresponding slider item when a pagination button is clicked
+    pagination.addEventListener('click', function(e) {
+      if (e.target.matches('.pagination__button')) {
+          var index = paginationItems.indexOf(e.target.parentNode);
+          msImages.select(index);
+      }
+    });
+  }
+}
 </script>
